3bb976ee6557103a2817e6f401f3a1bbb0cc0221
kimzool <lusiu9757@gmail.com>
2026-03-30 15:45:53 +0900

    배열

---
+++ array/array-test1.c
@@ -0,0 +1,29 @@
+﻿
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+
+
+	char s[] = "ABCD";
+	int a = 0, b = 3;
+	char t;
+
+	while (a < b) { // 0 3 번째 교환 - > 1  2 번째 교환
+		t = s[a];
+		s[a] = s[b];
+		s[b] = t;
+		a++;
+		b--;
+
+	}
+
+	printf("%s", s);
+	
+
+
+	return 0;
+}
