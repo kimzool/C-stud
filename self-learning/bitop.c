e95eb735613dccd6de61be0d21ba2e700a669e5f
kimzool <lusiu9757@gmail.com>
2026-04-01 14:55:47 +0900

    연산자 문제 1,2,3

---
+++ self-learning/bitop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main() {
+
+
+	int a = 8;
+	printf("%d %d %d", a << 1, a << 1, a & 3);
+
+
+	return 0;
+}
---
+++ self-learning/type.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+
+	int a = 7, b = 2;
+	int c;
+	double d, e;
+	c = a / b;
+	d = a / b;
+	e = (double)a / b;
+
+	printf("c = %d\n", c);
+	printf("d = %d\n", d);
+	printf("e = %d\n", e);
+
+
+
+	return 0;
+}
